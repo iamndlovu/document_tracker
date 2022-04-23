ac64d61707af5c228e86f52359d59507805c9dba
iamndlovu <pndlovu385@gmail.com>
2022-04-23 09:17:00 +0200

    responsive header & footer component

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import headerStyles from './Header.module.scss';
+
+const Header = () => {
+	//menu state
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	return (
+		<header className={headerStyles.Header}>
+			<div
+				className={`${headerStyles.menuButton} ${
+					isMenuOpen && headerStyles.open
+				}`}
+				onClick={() => setIsMenuOpen(!isMenuOpen)}
+			>
+				<div className={headerStyles.buttonLine}></div>
+				<div className={headerStyles.buttonLine}></div>
+				<div className={headerStyles.buttonLine}></div>
+			</div>
+			<nav>
+				<div>
+					<NavLink to="/">
+						<span className={headerStyles.Elephant}>Document</span>{' '}
+						<span className={headerStyles.AvantGarde}>Tracker</span>
+					</NavLink>
+				</div>
+				<ul className={`${isMenuOpen && headerStyles.open}`}>
+					<li>
+						<NavLink
+							to="/"
+							exact
+							className={({ isActive }) => isActive && headerStyles.active}
+						>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/about"
+							className={({ isActive }) => isActive && headerStyles.active}
+						>
+							About
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/login"
+							className={({ isActive }) => isActive && headerStyles.active}
+						>
+							Log In
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/contact"
+							className={({ isActive }) => isActive && headerStyles.active}
+						>
+							Report a bug
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
